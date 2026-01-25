86c49ee51845d0c7eb6f015e963d8a94a998fd43
Daniel <online.danielakinschin@gmail.com>
2026-01-25 15:51:13 +0100

    update

--- src/constants/artist.js
+++ src/constants/artist.js
@@ -1,2 +1,2 @@
-export const IMAGES_ROOT = '/src/art'
+export const IMAGES_ROOT = import.meta.env.PROD ? '/art' : '/src/art'
 export const IMAGES_FILES = ['a.png', 'b.png', 'c.png', '4k.png', 'eft.png']
