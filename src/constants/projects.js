a8c62a405dea364250c37276ceb138c520213ee3
Daniel <online.danielakinschin@gmail.com>
2026-01-25 11:49:52 +0100

    update

---
+++ src/constants/projects.js
@@ -0,0 +1,128 @@
+export const PROJECTS = [
+  {
+    meta: {
+      name: 'Lorem',
+      date: '26.01.2026',
+    },
+    files: {
+      path: './lorem',
+      thumbnail: 'a.png',
+      images: ['a.png', 'b.png', 'c.png'],
+    },
+    style: {
+      color: '#9376e4',
+    },
+  },
+  {
+    meta: {
+      name: 'Lorem',
+      date: '26.01.2026',
+    },
+    files: {
+      path: './lorem',
+      thumbnail: 'b.png',
+      images: ['a.png', 'b.png', 'c.png'],
+    },
+    style: {
+      color: '#9376e4',
+    },
+  },
+  {
+    meta: {
+      name: 'Lorem',
+      date: '26.01.2026',
+    },
+    files: {
+      path: './lorem',
+      thumbnail: 'c.png',
+      images: ['a.png', 'b.png', 'c.png'],
+    },
+    style: {
+      color: '#9376e4',
+    },
+  },
+  {
+    meta: {
+      name: 'Lorem',
+      date: '26.01.2026',
+    },
+    files: {
+      path: './lorem',
+      thumbnail: 'a.png',
+      images: ['a.png', 'b.png', 'c.png'],
+    },
+    style: {
+      color: '#9376e4',
+    },
+  },
+  {
+    meta: {
+      name: 'Lorem',
+      date: '26.01.2026',
+    },
+    files: {
+      path: './lorem',
+      thumbnail: 'b.png',
+      images: ['a.png', 'b.png', 'c.png'],
+    },
+    style: {
+      color: '#9376e4',
+    },
+  },
+  {
+    meta: {
+      name: 'Lorem',
+      date: '26.01.2026',
+    },
+    files: {
+      path: './lorem',
+      thumbnail: 'c.png',
+      images: ['a.png', 'b.png', 'c.png'],
+    },
+    style: {
+      color: '#9376e4',
+    },
+  },
+  {
+    meta: {
+      name: 'Lorem',
+      date: '26.01.2026',
+    },
+    files: {
+      path: './lorem',
+      thumbnail: 'a.png',
+      images: ['a.png', 'b.png', 'c.png'],
+    },
+    style: {
+      color: '#9376e4',
+    },
+  },
+  {
+    meta: {
+      name: 'Lorem',
+      date: '26.01.2026',
+    },
+    files: {
+      path: './lorem',
+      thumbnail: 'b.png',
+      images: ['a.png', 'b.png', 'c.png'],
+    },
+    style: {
+      color: '#9376e4',
+    },
+  },
+  {
+    meta: {
+      name: 'Lorem',
+      date: '26.01.2026',
+    },
+    files: {
+      path: './lorem',
+      thumbnail: 'c.png',
+      images: ['a.png', 'b.png', 'c.png'],
+    },
+    style: {
+      color: '#9376e4',
+    },
+  },
+]
