57b55358cd9d3bad758d588630e67164416f88da
Daniel <online.danielakinschin@gmail.com>
2026-01-25 14:31:44 +0100

    test update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,4 +2,7 @@
   <BaseSection class="d-flex flex-column justify-center">
     <AppGalery />
   </BaseSection>
+  <BaseSection class="d-flex flex-column justify-center">
+    <AppGalery />
+  </BaseSection>
 </template>
