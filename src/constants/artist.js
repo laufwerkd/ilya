7ec83fc414d6db1df4ebd25a2c3f3bdb3461e564
sayuri <issimon6539@gmail.com>
2026-02-12 01:54:40 +0700

    update

--- src/constants/artist.js
+++ src/constants/artist.js
@@ -1,9 +1,9 @@
 export const IMAGES = {
   root: import.meta.env.PROD ? '/art' : '/src/art',
   projects: {
-    project0: ['3d model.png', 'all sketches.png'],
-    project1: ['b -front.png', 'b all.png', 'b front.png'],
-    project2: ['c main.png', 'cranes sketch.png'],
+    project0: ['b front.png', 'b -front.png', 'b all.png'],
+    project1: ['c main.png', 'c 3d.png', 'c sketch.png'],
+    project2: ['all sketches.png'],
   },
 }
 export const SOCIALS = [
